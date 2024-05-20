ac58ea018ec5f04db20966b334f9950fb74985f9
Nitish John Toppo <Nitish.Toppo@Geminisolutions.com>
2024-05-20 18:06:28 +0530

    tweet

--- pages/page3.py
+++ pages/page3.py
@@ -4,17 +4,19 @@ import streamlit as st
 # Read the CSV sheet
 data = pd.read_csv('Sentiment.csv')
 
-# Display title and headers
+# Display title and headers with clear column renaming
 st.title("Sentiment Analysis")
-st.write(data[["text", "sentiment","compound"]].head(10))  # Show only first 10 rows
+data.rename(columns={"compound": "Sentiment Score"}, inplace=True)
+data.rename(columns={"text": "Tweets"}, inplace=True)
+data.rename(columns={"sentiment": "Sentiment"}, inplace=True)# Rename in-place
+st.write(data[["Tweets", "Sentiment", "Sentiment Score"]].head(10))  # Show first 10 rows
 
 # Function to display comments with sentiment coloring
 def display_sentiment(text, sentiment):
-  if sentiment == "positive":
-    st.success(text)
-  elif sentiment == "negative":
-    st.error(text)
-  else:
-    st.write(text)
+    if sentiment == "positive":
+        st.success(text)
+    elif sentiment == "negative":
+        st.error(text)
+    else:
+        st.write(text)
 
-# Display first 10 comments with sentiment colorin
